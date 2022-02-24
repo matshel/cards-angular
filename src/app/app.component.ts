a4807462075e56b3213afbb74ced6f44c1eaf04d
matshel <mats.helgesen@protonmail.com>
2022-02-24 01:15:59 +0100

    commit update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imgUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today during my hike',
+    },
+    {
+      title: 'Snowy Mountain',
+      imgUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      imgUrl: 'assets/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some hiking today',
+    },
+  ];
 }
